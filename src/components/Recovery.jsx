2b551cfa3a9893a2870e1eb981e10591b69d2b2d
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-02-04 18:18:27 +0530

    Component: Recovery

---
+++ src/components/Recovery.jsx
@@ -0,0 +1,70 @@
+import React, { useEffect, useState } from "react";
+/*
+import { Link } from "react-router-dom";
+import avatar from "../assets/profile.png";
+*/
+import styles from "../styles/Username.module.css";
+import toast, { Toaster } from "react-hot-toast";
+import { useAuthStore } from "../store/store";
+import { generateOTP, verifyOTP } from "../helper/apiRequest";
+import { useNavigate } from "react-router-dom";
+/* 
+import { useFormik } from "formik";
+import { validatePassword } from "../helper/validate";
+*/
+
+const Recovery = () => {
+  const username = useAuthStore((state) => state.auth);
+
+  const [otp, setOtp] = useState();
+
+  return (
+    <div className="container mx-auto">
+      <Toaster position="top-center" reverseOrder={false}></Toaster>
+      <div className="flex justify-center items-center h-screen">
+        <div className={styles.glass}>
+          <div className="title flex flex-col items-center">
+            <h4 className="text-5xl font-bold">Recovery</h4>
+            <span className="py-5 text-xl w-2/3 text-center text-gray-500">
+              Enter OTP to recover password.
+            </span>
+          </div>
+
+          <form className="pt-20" onSubmit={onSubmitHandler}>
+            <div className="textbox flex flex-col items-center gap-6">
+              <div className="input text-center">
+                <span className="py-4 text-sm text-left text-gray-500">
+                  Enter 6 digit OTP sent to your email address.
+                </span>
+                <input
+                  type="number"
+                  className={styles.textbox}
+                  name="otp"
+                  id="otp"
+                  placeholder="OTP"
+                  onChange={(e) => {
+                    setOtp(e.target.value);
+                  }}
+                />
+              </div>
+              <button type="submit" className={styles.btn}>
+                Recover
+              </button>
+            </div>
+          </form>
+
+          <div className="text-center py-4">
+            <span className="text-gray-500">
+              Can't get OTP?{" "}
+              <button className="text-red-500" onClick={resendOTP}>
+                Resend
+              </button>
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Recovery;
