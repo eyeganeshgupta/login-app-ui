e5ae45c39b1992e8a2f194119680c402c04691d2
eyeganeshgupta <eyeganeshgupta@gmail.com>
2024-02-06 05:38:31 +0530

    validate-reset-password

--- src/components/Reset.jsx
+++ src/components/Reset.jsx
@@ -1,8 +1,41 @@
 import React from "react";
 import styles from "../styles/Username.module.css";
 import toast, { Toaster } from "react-hot-toast";
+import { useFormik } from "formik";
+import { validateResetPassword } from "../helper/validate";
+import { resetPassword } from "../helper/apiRequest.js";
+import { useAuthStore } from "../store/store.js";
+import { Navigate, useNavigate } from "react-router-dom";
+import useFetch from "../hooks/useFetch.js";
 
 const Reset = () => {
+  const navigate = useNavigate();
+  const { username } = useAuthStore((state) => state.auth);
+
+  const [{ isLoading, status, serverError }] = useFetch("create-reset-session");
+
+  const formik = useFormik({
+    initialValues: {
+      password: "",
+      confirmPassword: "",
+    },
+    validate: validateResetPassword,
+    validateOnBlur: false,
+    validateOnChange: false,
+  });
+
+  if (isLoading) {
+    return <h1 className="text-2xl font-bold">Loading...</h1>;
+  }
+
+  if (serverError) {
+    return <h1 className="text-xl text-red-500">{serverError?.message}</h1>;
+  }
+
+  if (status && status !== 201) {
+    return <Navigate to={"/password"} replace={true}></Navigate>;
+  }
+
   return (
     <div className="container mx-auto">
       <Toaster position="top-center" reverseOrder={false}></Toaster>
